6583d3104dace3590066c4c610c56a0d6b61bed7
veritakim <kjs3597@gmail.com>
2022-08-30 18:51:49 +0900

    :memo: feat: login
    feat: login

--- off-server/src/apis/auths/auths.module.ts
+++ off-server/src/apis/auths/auths.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtAccessStrategy } from 'src/commons/auth/jwt-access.strategy';
 import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
 import { AuthsResolver } from './auths.resolver';
+import { AuthsService } from './auths.service';
 import { Token } from './entities/token.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Token, User])],
   providers: [
     AuthsResolver, //
+    AuthsService,
+    JwtService,
+    UsersService,
   ],
 })
 export class AuthsModule {}
--- off-server/src/apis/auths/auths.resolver.ts
+++ off-server/src/apis/auths/auths.resolver.ts
@@ -1,30 +1,42 @@
-import { CACHE_MANAGER, Inject, UnprocessableEntityException } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  Inject,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { IContext } from 'src/commons/type/context';
 import { AuthsService } from './auths.service';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
+import { UsersService } from '../users/users.service';
+import { Cache } from 'cache-manager';
 
 @Resolver()
 export class AuthsResolver {
   constructor(
-    // private readonly userService: UsersService,
+    private readonly userService: UsersService,
     private readonly authsService: AuthsService,
-    @Inject(CACHE_MANAGER)
-    private readonly cacheManager: Cache,
   ) {}
-  // query
 
   // mutation
   @Mutation(() => String)
-  async login (
+  async login(
     @Args('email') email: string,
     @Args('password') password: string,
     @Context() context: IContext,
   ) {
-    // const user = await userService.fetchUser(email)
-    // const isAuth = await bcrypt.compare(password, user.password)
-    
-    // if ( !isAuth || !user ) throw new UnprocessableEntityException('이메일이나 비밀번호를 확인해주세요')
-  }
+    const user = await this.userService.findOne({ id: email });
+    if (!user)
+      throw new UnprocessableEntityException(
+        '이메일이나 비밀번호를 확인해주세요',
+      );
+    const isAuth = await bcrypt.compare(password, user.password);
 
+    if (!isAuth)
+      throw new UnprocessableEntityException(
+        '이메일이나 비밀번호를 확인해주세요',
+      );
+
+    this.authsService.setRefreshToken({ user, res: context.res });
+    return this.authsService.getAccessToken({ user });
+  }
 }
