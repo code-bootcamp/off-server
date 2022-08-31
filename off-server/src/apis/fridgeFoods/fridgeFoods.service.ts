4ed1ff1b93eec994b4dafd519139cc685cea2d6c
veritakim <kjs3597@gmail.com>
2022-08-31 18:42:54 +0900

    :sparkles: feat: fridgeFood delete
    feat: fridgeFood delete

--- off-server/src/apis/fridgeFoods/fridgeFoods.service.ts
+++ off-server/src/apis/fridgeFoods/fridgeFoods.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnprocessableEntityException } from "@nestjs/common";
+import { Args } from "@nestjs/graphql";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { FridgesService } from "../fridges/fridges.service";
@@ -20,5 +21,17 @@ export class FridgeFoodsService {
       fridge: myFridge.id,
       ...fridgeFoodInput
     })
+  }
+
+  async deleteFood({fridgeId, foodId, userId}){
+    // user의 냉장고음식인지 check
+    const fridge = await this.fridgesService.findOne(userId)
+
+    if (fridgeId !== fridge.id) {
+      throw new UnprocessableEntityException('권한이 없습니다.')
+    }
+
+    const result = await this.fridgeFoodRepository.softDelete({id: foodId})
+    return result.affected ? true : false
   }
 }
