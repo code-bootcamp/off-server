fd18af793a88c49f47151f75503f6aca2b06d059
veritakim <kjs3597@gmail.com>
2022-09-05 16:18:33 +0900

    :rotating_light: fix: cors error
    fix: cors error

--- off-server/src/main.ts
+++ off-server/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://freshfridge.shop:3000'],
+    credentials: true, 
+  });
   app.use(graphqlUploadExpress());
   await app.listen(3000);
 }
