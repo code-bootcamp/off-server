ed2dd0827bee3b23a6f1702c42fa817959e59f03
juhyunju <2juhyunju@naver.com>
2022-09-13 15:12:57 +0900

    :sparkles: feat:fetch category
    feat:fetch category

--- off-server/src/apis/category/category.resolver.ts
+++ off-server/src/apis/category/category.resolver.ts
@@ -1,18 +1,17 @@
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
-import { CategoryService } from "./category.service";
-import { Category } from "./entities/category.entity";
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CategoryService } from './category.service';
+import { Category } from './entities/category.entity';
 
 @Resolver()
 export class CategoryResolver {
-  constructor(
-    private readonly categoryService: CategoryService,
-  ){}
-  
+  constructor(private readonly categoryService: CategoryService) {}
+
   @Mutation(() => Category)
-  async createCategory(
-    @Args('name') name: string
-  ){
-    return await this.categoryService.create({name})
-    
-  }  
+  async createCategory(@Args('name') name: string) {
+    return await this.categoryService.create({ name });
+  }
+  @Query(() => [Category])
+  fetchCategory() {
+    return this.categoryService.findAll();
+  }
 }
